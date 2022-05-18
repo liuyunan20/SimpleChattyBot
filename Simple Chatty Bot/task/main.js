d05f37c3b11902cfe0c8674997be3be194094186
liuyunan <liuyunan.20@gmail.com>
2022-05-18 11:29:12 -0700

    At this stage, you will introduce yourself to the bot. It will greet you by your name and then try to guess your age using arithmetic operations.
    Your program should print the following lines:
    
    Hello! My name is Aid.
    I was created in 2022.
    Please, remind me your name.
    What a great name you have, Max!
    Let me guess your age.
    Enter remainders of dividing your age by 3, 5 and 7.
    Your age is {your_age}; that's a good time to start programming!
    Read three numbers from the standard input. Assume that all the numbers will be given on separate lines.
    
    Instead of {your_age}, the bot will print the age determined according to the special formula discussed above.

--- Simple Chatty Bot/task/main.js
+++ Simple Chatty Bot/task/main.js
@@ -1,9 +1,18 @@
 const input = require('sync-input');
 
 console.log("Hello! My name is Aid.");
-console.log("I was created in 2022.");
+console.log("I was created in 2020.");
 console.log("Please, remind me your name.");
 
-// use input() + to get the user's output
 let name = input();
-console.log(`What a great name you have, ${name}!`);
+
+console.log("What a great name you have, " + name + "!");
+console.log("Let me guess your age.");
+console.log("Enter remainders of dividing your age by 3, 5 and 7.");
+
+// reading all remainders
+let remainder3 = input();
+let remainder5 = input();
+let remainder7 = input();
+let age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+console.log("Your age is " + age + "; that's a good time to start programming!");
